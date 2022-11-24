3cc05677f9be0be6ce92a05d0e6fc4723cb04d4b
mshahzad257 <114944060+mshahzad257@users.noreply.github.com>
2022-11-24 16:39:53 +0500

    vercel AB-Y

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const cors = require("cors");
 const http = require('http');
 const LinkModel = require('./models/link');
-
-// Load config
-dotenv.config({ path: './config/config.env' });
 
 // connect DB
 connectDB();
@@ -24,8 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-// Port assign
-const port = 5000 || 3000;
+app.use("/", express.static(path.join(__dirname, './frontend/build')));
+// app.use("/", express.static(path.resolve(path.join(__dirname, "frontend/build"))))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+// });
+
 
 app.get('/api', async (req, res) => {
     const links = await LinkModel.find();
@@ -47,12 +47,8 @@ app.post('/api', async (req, res) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, './frontend/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
-});
-
+const port = 5000 || 3000;
 
 http.createServer(app).on("error", (ex) => {
     console.log(ex);
