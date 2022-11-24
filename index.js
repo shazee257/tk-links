43a5e19f10fec1d5315161ca9a7cd4853c3ce684
mshahzad257 <114944060+mshahzad257@users.noreply.github.com>
2022-11-24 19:33:43 +0500

    vercel tested deployed

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const cors = require("cors");
-const http = require('http');
 const LinkModel = require('./models/link');
 
 // connect DB
@@ -11,21 +10,13 @@ connectDB();
 
 // app init
 const app = express();
-let server = http.createServer(app);
+const PORT = 5000 || 3000;
 
 // Middlewares
 app.use(cors());
-
-app.use(express.json({ limit: '200kb' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-// app.use("/", express.static(path.join(__dirname, './frontend/build')));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './frontend/build/index.html'));
-// });
 
 app.get('/api', async (req, res) => {
     const links = await LinkModel.find({});
@@ -47,17 +38,18 @@ app.post('/api', async (req, res) => {
     });
 });
 
-app.use("/", express.static(path.resolve(path.join(__dirname, "frontend/build"))))
-
-const PORT = 5000 || 3000;
-
-// http.createServer(app).on("error", (ex) => {
-//     console.log(ex);
-//     console.log("Can't connect to server.");
-// }).listen(port, () => {
-//     console.log(`Server Started :: ${port}`);
+// // Frontend Routes
+// app.use(express.static(path.join(__dirname, './frontend/build')));
+// app.get('*', function (_, res) {
+//     res.sendFile(path.join(__dirname, './frontend/build/index.html'),
+//         function (err) {
+//             if (err) {
+//                 res.status(500).send(err)
+//             }
+//         }
+//     )
 // });
 
-server.listen(PORT, () => {
-    console.log("Server is Running:", PORT);
+app.listen(PORT, () => {
+    console.log(`Server Started :: ${PORT}`);
 });
